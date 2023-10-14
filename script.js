bfb5ca48f760e3786ad2bf8f1bea6f56cf61e545
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-14 12:34:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function onesDigit(n) {
   // your code here
+	 if (Number.isInteger(number) && number > 0) {
+    return number % 10; // Using the modulo operator to get the ones digit
+  } else {
+    throw new Error("Input must be a positive integer.");
+  }
 }
 
 // Do not change the code below
